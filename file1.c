6d69f07b5487676489c805bbbc76746a800cadb3
dhanalakshmiviven <dhanalakshmithavva@gmail.com>
2024-11-21 14:58:37 +0530

    my commit

---
+++ file1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+struct{
+	int i;
+	float x;
+}var;
+struct var variable1;
+variable1.i=0;
+varaible1.x=9.7;
+printf("the values in structure are %d %f:\n",varaible1.i,variable1.y);
+}
